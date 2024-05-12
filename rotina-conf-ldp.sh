c2a3c395469002398636202fd7030a09ed5b4f50
Luan Andrade <61974621+Lu4nWV4@users.noreply.github.com>
2024-05-12 18:38:05 -0300

    rotina-conf-ldp.sh

--- rotina-conf-ldp.sh
+++ rotina-conf-ldp.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Author: Luan Andrade
+# Linkedin: https://www.linkedin.com/in/luan-william-vieira-de-andrade-5a8110197/
+#
 
 for ip in `cat lista-ip-sw-hw.txt | sort -n |  uniq`; 
 do
